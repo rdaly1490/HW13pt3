ceae47cc4981db6e47c3122155be9179a38e9543
Rob <rdaly1490@gmail.com>
2015-05-26 13:55:33 -0500

    tabs hard mode done

--- dist/main.js
+++ dist/main.js
@@ -18,27 +18,36 @@ $gText.hide();
 $red.on("click", hideBG);
 $blue.on("click", hideRG);
 $green.on("click", hideRB);
-$nav.on("click", activeTab);
+// $nav.on("click", activeTab);
 
 function hideBG(e) {
 	$bText.hide();
 	$gText.hide();
 	$rText.show();
+	$red.css("backgroundColor", "gray");
+	$blue.css("backgroundColor", "white");
+	$green.css("backgroundColor", "white");
 }
 function hideRG(e) {
 	$rText.hide();
 	$gText.hide();
 	$bText.show();
+	$red.css("backgroundColor", "white");
+	$blue.css("backgroundColor", "gray");
+	$green.css("backgroundColor", "white");
 }
 function hideRB(e) {
 	$rText.hide();
 	$bText.hide();
 	$gText.show();
+	$red.css("backgroundColor", "white");
+	$blue.css("backgroundColor", "white");
+	$green.css("backgroundColor", "gray");
 }
 
-function activeTab(e) {
-	e.target.style.backgroundColor = "gray";
-}
+// function activeTab(e) {
+// 	e.target.style.backgroundColor = "gray";
+// }
 
 $rText.css("border", "1px solid black");
 $bText.css("border", "1px solid black");
